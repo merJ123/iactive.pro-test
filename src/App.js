62ebd83e6aeb7e3afd750a810285400f212c689e
merJ123 <meir.balakhmet@icloud.com>
2022-05-04 02:27:42 +0600

    deployed website

--- src/App.js
+++ src/App.js
@@ -7,27 +7,7 @@ import MessageFeed from './components/MessageFeed';
 
 function App() {
   
-  const [data, setData] = React.useState([
-    {
-      attachments: [
-        {
-          type:'video',
-          url: "http://media.iactive.pro/ZTvhtW44WDYrU4R3wIOB/messages_videos/telegrambot/dSWifIUl6eICdsv2EBZgcJx1Jon9Uwvp.mp4"
-        },
-        {
-          type: 'img',
-          url: 'https://ru.reactjs.org/logo-og.png'
-        }
-      ],
-      author: "Хорошие новости",
-      channel: "telegrambot",
-      content: "Кубанские токари давно научились изготавливать то, что все привыкли ждать из-за границы. Небольшой токарный цех выполняет заказы по замене деталей на импортной технике — что не только дешевле, но и быстрее в производстве⚙️",
-      date: "2022-04-03 20:51:03",
-      id: "2682",
-      region: "",
-      senderNumber: "1001692672105"
-    }
-  ])
+  const [data, setData] = React.useState([])
 
   useEffect(() => {
     axios({
